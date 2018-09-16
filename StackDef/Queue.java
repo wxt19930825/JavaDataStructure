762c9230728a96d15b2bc7c3de3993bbd0a8320d
wxt19930825 <494187797@qq.com>
2018-09-16 17:43:28 +0800

    Signed-off-by: wxt19930825 <494187797@qq.com>

--- StackDef/Queue.java
+++ StackDef/Queue.java
@@ -11,14 +11,14 @@ public class Queue {
 	}
 	
 	public Object pop() throws Exception{
-		if (stack2.getSize()<=0) {
-			while (stack1.getSize() >0) {
+		if (stack2.getTop()==-1) {
+			while (stack1.getTop() !=-1) {
 				Object obj = stack1.pop();
 				stack2.push(obj);
 			}
 		}
 		
-		if (stack2.getSize() <=0) {
+		if (stack2.getTop() == -1) {
 			throw new Exception("queue is empty");
 		}
 		return stack2.pop();
@@ -27,5 +27,21 @@ public class Queue {
 	
 	public Object push(Object obj){
 		return stack1.push(obj);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Queue queue = new Queue();
+		queue.push(1);
+		queue.push(2);
+		queue.push(3);
+		queue.push(4);
+		queue.push(5);
+		
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
 	}
 }
